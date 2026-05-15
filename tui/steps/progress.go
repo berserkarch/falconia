a9715d938262d8cae9bce0096396d54508d666ba
Gaurav Raj <gauravraj0408@gmail.com>
2026-05-15 20:57:07 +0530

    fix ui

--- tui/steps/progress.go
+++ tui/steps/progress.go
@@ -351,10 +351,10 @@ func (m ProgressModel) viewRunning() string {
 		pct = float64(m.current) / float64(len(m.steps))
 	}
 	b.WriteString(style.ProgressBar(60, pct) +
-		style.StyleMuted.Render(fmt.Sprintf("  %d%%\n\n", int(pct*100))))
+		style.StyleMuted.Render(fmt.Sprintf("  %d%%\n\n\n", int(pct*100))))
 
 	if len(m.logLines) > 0 {
-		b.WriteString(m.viewport.View() + "\n")
+		b.WriteString(m.viewport.View() + "\n\n")
 	}
 
 	return b.String()
